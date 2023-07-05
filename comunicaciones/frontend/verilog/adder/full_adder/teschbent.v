89f7e670a204aebb9a2f03ae4c01dd1f4dd42156
JPablo <juapabgonzalez@utp.edu.co>
2023-07-04 20:37:15 -0500

    testbench

--- comunicaciones/frontend/verilog/adder/full_adder/teschbent.v
+++ comunicaciones/frontend/verilog/adder/full_adder/teschbent.v
@@ -6,11 +6,9 @@ wire s,c;
 full_adder fa (s,c,a,b,cin);
 
 initial begin
-
     $dumpfile("full_adder.vcd");
     $dumpvars(0,full_adder_testbench);
     $monitor($time," a=%b b=%b cin=%b s=%b c=%b",$time,a,b,cin,s,c);
-
     a=0; b=0; cin=0;
     #10 a=0; b=0; cin=1;
     #10 a=0; b=1; cin=0;
@@ -20,6 +18,5 @@ initial begin
     #10 a=1; b=1; cin=0;
     #10 a=1; b=1; cin=1;
     #10 $finish;
-
 end                                                                                     
 endmodule
