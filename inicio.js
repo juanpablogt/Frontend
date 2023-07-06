516ff0ff83e71a515240c369dece85291e92a393
JPablo <juapabgonzalez@utp.edu.co>
2023-07-06 18:55:01 -0500

    listas y ciclos.

--- inicio.js
+++ inicio.js
@@ -11,4 +11,12 @@ myColors.forEach(function(color){
     document.write(color+"<br>");
 }
 );
+let pets = [name="Apolo", type="dog", age=5, name="Luna", type="cat", age=2, name="Kiara", type="dog", age=3];
+
+pets.push(name="Perro", type="cat", age=2);
+
+pets.forEach(function(pet){
+    document.write(pet+"<br>");
+}
+);
 
