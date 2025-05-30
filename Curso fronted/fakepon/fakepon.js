b02ce73782e3d717ba11be8cf2874ed54227ac2a
juanpablogt <juapabgonzalez@utp.edu.co>
2025-05-30 10:53:04 -0500

    DOM javascript

--- Curso fronted/fakepon/fakepon.js
+++ Curso fronted/fakepon/fakepon.js
@@ -8,19 +8,42 @@ function seleccionarMascota() {
     let inputtortugon = document.getElementById("tortugon");
     let inputpajarito = document.getElementById("pajarito");
     let inputperrito = document.getElementById("perrito");
+    let spanMascotaJugador = document.getElementById("mascota-jugador");
 
     if (inputhipop.checked) {
-        alert("Has seleccionado Hipopótamo");
+        spanMascotaJugador.innerHTML = "Hipop";
     }else if (inputtortugon.checked) {
-        alert("Has seleccionado Tortugón");
+        spanMascotaJugador.innerHTML = "Tortugon";
     }else if (inputpajarito.checked) {
-        alert("Has seleccionado Pajarito");
+        spanMascotaJugador.innerHTML = "Pajarito";
     }
     else if (inputperrito.checked) {
-        alert("Has seleccionado Perrito");
+        spanMascotaJugador.innerHTML = "Perrito";
     }else {
         alert("Por favor, selecciona una mascota");
     }
+    seleccionarOponente();
+}
+
+function seleccionarOponente() {
+    let ataqueAleatorio = aleatorio(1, 4);
+    let spanMascotaOponente = document.getElementById("mascota-enemy");
+    if (ataqueAleatorio == 1) {
+        spanMascotaOponente.innerHTML = "Hipop";
+    }
+    else if (ataqueAleatorio == 2) {
+        spanMascotaOponente.innerHTML = "Tortugon";
+    }
+    else if (ataqueAleatorio == 3) {
+        spanMascotaOponente.innerHTML = "Pajarito";
+    }
+    else if (ataqueAleatorio == 4) {
+        spanMascotaOponente.innerHTML = "Perrito";
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 window.addEventListener("load", iniciar);
