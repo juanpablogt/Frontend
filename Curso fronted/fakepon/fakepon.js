e57795643644e98f0c3986b1a95fffd00938a49b
juanpablogt <juapabgonzalez@utp.edu.co>
2025-05-30 18:47:07 -0500

    perfeccionando la seccion de seleccion de mascota

--- Curso fronted/fakepon/fakepon.js
+++ Curso fronted/fakepon/fakepon.js
@@ -1,8 +1,10 @@
 let ataquejugador;
 let ataqueEnemy;
+let vidasJugador = 3;
+let vidasOponente = 3;
 
 function iniciar() {
-    
+
     let botonMascota = document.getElementById("boton-mascota");
     botonMascota.addEventListener("click", seleccionarMascota);
 
@@ -12,6 +14,8 @@ function iniciar() {
     botonWater.addEventListener("click", botonWaterClick);
     let botonGrass = document.getElementById("boton-grass");
     botonGrass.addEventListener("click", botonGrassClick);
+    let botonReset = document.getElementById("reinicio");
+    botonReset.addEventListener("click", resetearJuego);
 }
 
 function seleccionarMascota() {
@@ -22,14 +26,14 @@ function seleccionarMascota() {
     let spanMascotaJugador = document.getElementById("mascota-jugador");
 
     if (inputhipop.checked) {
-        spanMascotaJugador.innerHTML = "Hipop";
+        spanMascotaJugador.innerHTML = "Hipop ";
     }else if (inputtortugon.checked) {
-        spanMascotaJugador.innerHTML = "Tortugon";
+        spanMascotaJugador.innerHTML = "Tortugon ";
     }else if (inputpajarito.checked) {
-        spanMascotaJugador.innerHTML = "Pajarito";
+        spanMascotaJugador.innerHTML = "Pajarito ";
     }
     else if (inputperrito.checked) {
-        spanMascotaJugador.innerHTML = "Perrito";
+        spanMascotaJugador.innerHTML = "Perrito ";
     }else {
         alert("Por favor, selecciona una mascota");
     }
@@ -87,13 +91,62 @@ function ataquealeaEnemy() {
 
 function createMensaje() {
     let sectionMensajes = document.getElementById("mensajes");
-    let parrafo = document.createElement("p")
-    parrafo.innerHTML = "Tu mascota ataca con " + ataquejugador + ", la mascota enemiga ataca con " + ataqueEnemy;
-    sectionMensajes.appendChild(parrafo);
+    sectionMensajes.innerHTML = "Tu ataque: " + ataquejugador + "<br> Ataque enemigo: " + ataqueEnemy;
+    combate();
 }
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function combate() {
+    let sapanVidasJugador = document.getElementById("vidas-jugador");
+    let spanVidasOponente = document.getElementById("vidas-enemy");
+    let spanMascotaJugador = document.getElementById("mascota-jugador");
+    let spanMascotaOponente = document.getElementById("mascota-enemy");
+    let sectionMensajes = document.getElementById("mensajes");
+    
+    if (ataquejugador == ataqueEnemy) {
+        sectionMensajes.innerHTML = "Empate";
+    } else if (
+        (ataquejugador == "Fuego" && ataqueEnemy == "Planta") ||
+        (ataquejugador == "Agua" && ataqueEnemy == "Fuego") ||
+        (ataquejugador == "Planta" && ataqueEnemy == "Agua")
+    ) {
+        sectionMensajes.innerHTML = "Ganaste! " + spanMascotaJugador.innerHTML + " ataca con " + ataquejugador + ", y " + spanMascotaOponente.innerHTML + " ataca con " + ataqueEnemy;
+        vidasOponente--;
+        spanVidasOponente.innerHTML = vidasOponente;
+    } else {
+        sectionMensajes.innerHTML = "Perdiste! " + spanMascotaJugador.innerHTML + " ataca con " + ataquejugador + ", y " + spanMascotaOponente.innerHTML + " ataca con " + ataqueEnemy;
+        vidasJugador--;
+        sapanVidasJugador.innerHTML = vidasJugador;
+        
+    }
+    contadorVidas();
+
+}
+function contadorVidas() {
+    let sapanVidasJugador = document.getElementById("vidas-jugador");
+    let spanVidasOponente = document.getElementById("vidas-enemy");
+
+    if (vidasJugador <= 0) {
+        alert("Perdiste todas tus vidas, el juego ha terminado");
+        sapanVidasJugador.innerHTML = 0;
+        resetearJuego();
+    } else if (vidasOponente <= 0) {
+        alert("Felicidades, has ganado el juego!");
+        spanVidasOponente.innerHTML = 0;
+        resetearJuego();
+    }
+}
+function resetearJuego() {
+    let sectionMensajes = document.getElementById("mensajes");
+    sectionMensajes.innerHTML = "";
+    vidasJugador = 3;
+    vidasOponente = 3;
+    let sapanVidasJugador = document.getElementById("vidas-jugador");
+    let spanVidasOponente = document.getElementById("vidas-enemy");
+    sapanVidasJugador.innerHTML = vidasJugador;
+    spanVidasOponente.innerHTML = vidasOponente;
+}
 window.addEventListener("load", iniciar);
