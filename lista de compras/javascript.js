a831b3d9771f6d0e4be253217b6ebada4cb253a6
juanpablogt <juapabgonzalez@utp.edu.co>
2025-05-31 20:54:54 -0500

    cambios estilos, pendiente cambios en js

--- lista de compras/javascript.js
+++ lista de compras/javascript.js
@@ -1,19 +1,22 @@
-let ourform = document.getElementById("ourform");
-let ourfield = document.getElementById("ourfield");
-let ourlist = document.getElementById("ourlist");
+window.addEventListener("load", iniciar);
+function iniciar() {
+    let ourform = document.getElementById("ourform");
+    let ourfield = document.getElementById("ourfield");
+    let ourlist = document.getElementById("ourlist");
 
-ourform.addEventListener("submit", (e) => {
-    e.preventDefault();
-    createItem(ourfield.value);
-});
+    ourform.addEventListener("submit", (e) => {
+        e.preventDefault();
+        createItem(ourfield.value);
+    });
 
-function createItem(x) {
-    let ourHTML = `<li>${x} <button onclick="deleteItem(this)">Delete</button></li>`;
-    ourlist.insertAdjacentHTML("beforeend", ourHTML);
-    ourfield.value = "";
-    ourfield.focus();
-}
+    function createItem(x) {
+        let ourHTML = `<li>${x} <button onclick="deleteItem(this)">Delete</button></li>`;
+        ourlist.insertAdjacentHTML("beforeend", ourHTML);
+        ourfield.value = "";
+        ourfield.focus();
+    }
 
-function deleteItem(elementToDelete) {
-    elementToDelete.parentElement.remove();
+    window.deleteItem = function(elementToDelete) {
+        elementToDelete.parentElement.remove();
+    };
 }
