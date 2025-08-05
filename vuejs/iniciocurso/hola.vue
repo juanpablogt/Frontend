7f8a921b86c55b23549464ca5fa6437583c88b6e
juanpablogt <juapabgonzalez@utp.edu.co>
2025-08-04 21:52:31 -0500

    curso de vue

---
+++ vuejs/iniciocurso/hola.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>lorem ipsum</p>
+        <div>{{ message }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello, Vue!'
+        };
+    }
+};
+</script>
+<style>
+p {
+    color: blue;
+    font-size: 20px;
+}
+</style>
